7581db99bf6c5a69f1bbd0f8d596810fb1d5f146
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-29 16:44:30 +0530

    done with the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Button } from '@/components/ui/button'
 import { WavyBackground } from "@/components/global/waves";
 import { products } from "@/lib/constants";
 import { HeroParallax } from "@/components/global/connect-parallax";
+import { LampComponent } from '@/components/global/lamp';
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col">
@@ -44,7 +46,19 @@ export default function Home() {
       <section className="bg-black">
         <HeroParallax products={products}></HeroParallax>
       </section>
-     
+      <section className="mt-[0px] bg-black w-full"> 
+      <LampComponent/>
+      <Link
+          href="#"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            create here
+          </span>
+        </Link>
+      </section>
+    
     </main>
   );
 }
