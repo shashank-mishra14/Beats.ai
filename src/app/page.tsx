0d4c7607a0e8b1501420f637a51540fbed3a944a
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-30 18:27:31 +0530

    main the root directory of the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,18 +47,11 @@ export default function Home() {
         <HeroParallax products={products}></HeroParallax>
       </section>
       <section className="mt-[0px] bg-black w-full"> 
-      <LampComponent/>
-      <Link
-          href="#"
-          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
-        >
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            create here
-          </span>
-        </Link>
-      </section>
+      <LampComponent
+      />
     
+      </section>
+     
     </main>
   );
 }
--- src/components/global/lamp.tsx
+++ src/components/global/lamp.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { cn } from '@/lib/utils'
 import { SparklesCore } from './sparkles'
-
+import Link from 'next/link'
 export function LampComponent() {
   return (
     <LampContainer>
@@ -19,6 +19,15 @@ export function LampComponent() {
       >
         Music for you <br />By you
       </motion.h1>
+      <Link
+          href="/beatsSection"
+          className="relative inline-flex h-14 overflow-hidden rounded-full p-[2px]  mt-10 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-[1.25rem] font-medium text-white backdrop-blur-3xl">
+            create here
+          </span>
+        </Link>
     </LampContainer>
   )
 }
@@ -33,11 +42,11 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        'relative flex min-h-[800px] flex-col items-center justify-center overflow-hidden bg-neutral-950 w-full rounded-md z-0',
+        'relative flex min-h-[800px] pt-10 flex-col items-center justify-center overflow-hidden bg-neutral-950 w-full rounded-md z-0',
         className
       )}
     >
-      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
+      <div className="relative flex w-full flex-1 scale-y-125 mt-[10rem] items-center justify-center isolate z-0 ">
         <motion.div
           initial={{ opacity: 0.5, width: '15rem' }}
           whileInView={{ opacity: 1, width: '30rem' }}
