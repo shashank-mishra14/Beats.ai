db23ca7fffd469539545da50d103969b507049b2
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-06-30 22:55:44 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,12 +26,12 @@ export default function Home() {
       <ContainerScroll
       titleComponent={
         <div className="flex items-center flex-col mt-[-100px]">
-          <Button
+          <Button 
             size={'lg'}
             className="p-8 mb-8 md:mb-[150px] text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
           >
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-            create beats
+              create beats
             </span>
           </Button>
           <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
--- src/components/global/lamp.tsx
+++ src/components/global/lamp.tsx
@@ -20,7 +20,7 @@ export function LampComponent() {
         Music for you <br />By you
       </motion.h1>
       <Link
-          href="#"
+          href="/beatsSection"
           className="relative inline-flex h-14 overflow-hidden rounded-full p-[2px]  mt-10 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
